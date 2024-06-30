777a62cb97c25115986484f074e3b7cb5c80e8b6
Philip De Smedt <philip.desmedt@gmail.com>
2024-06-30 14:57:24 +0200

    wip

--- web/src/app/components/Center.tsx
+++ web/src/app/components/Center.tsx
@@ -4,6 +4,7 @@
 
 import { useEffect, useState } from 'react';
 import { PlaceholderBar } from '@/app/components/PlaceholderBar';
+import { currency } from '@/app/common/utils';
 
 interface CenterProps {
   className: string;
@@ -11,9 +12,37 @@ interface CenterProps {
 
 export function Center({ className }: CenterProps) {
   const [isLoading, setIsLoading] = useState(true);
+  const [startedLoading, setStartedLoading] = useState(false);
+  const [amountLocked, setAmountLocked] = useState<number>(0);
+
+  const fetchStxPrice = async () => {
+    const url = 'https://laozi1.bandchain.org/api/oracle/v1/request_prices?ask_count=16&min_count=10&symbols=STX';
+    const response = await fetch(url, { cache: 'no-store' });
+    const data = await response.json();
+    if (data['price_results']?.length > 0) {
+      const priceSTX = data['price_results'][0]['px'] / Number(data['price_results'][0]['multiplier']);
+      return priceSTX;
+    }
+
+    return 0;
+  }
+
+  const loadPoxData = async () => {
+    const url = 'https://api.mainnet.hiro.so/v2/pox';
+    const response = await fetch(url, { credentials: 'omit' });
+    const data = await response.json();
+
+    const stxPrice = await fetchStxPrice();
+    const stxStacked = Number(data['current_cycle']['stacked_ustx']) / 1000000.0;
+
+    setAmountLocked(stxPrice * stxStacked);
+    setIsLoading(false);
+  };
 
   useEffect(() => {
+    if (startedLoading) return;
 
+    loadPoxData();
   }, []);
 
   return (
@@ -38,7 +67,7 @@ export function Center({ className }: CenterProps) {
           />
         </div>
       ) : (
-        <span>Currently there is $622,000,000 locked up in PoX.</span>
+        <span>Currently there is ${currency.short.format(amountLocked)} locked up in PoX.</span>
       )}
     </div>
   );
