4872ce8d3add42733992ea74a202a11c585efb01
Philip De Smedt <philip.desmedt@gmail.com>
2024-06-30 23:34:39 +0200

    add liquid stacking

--- web/src/app/components/Footer.tsx
+++ web/src/app/components/Footer.tsx
@@ -5,7 +5,7 @@ export function Footer() {
 
   return (
     <main className="flex flex-col items-center justify-between p-24">
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
+      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-5 lg:text-left">
         <a
           href="#"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
@@ -37,6 +37,23 @@ export function Footer() {
           </h2>
           <p className="m-0 max-w-[30ch] text-sm opacity-50">
             Learn about PoX, how it powers Stacks and data insights.
+          </p>
+        </a>
+
+        <a
+          href="#"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className="mb-3 text-2xl font-semibold">
+            Liquid Stacking{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className="m-0 max-w-[30ch] text-sm opacity-50">
+            Learn about liquid stacking and how you can use it on Stacks
           </p>
         </a>
 
