7859ab6658e426934bcc03f79a2e7bbf4daf6c2f
Niel Deckx <hello@nieldeckx.be>
2024-07-04 10:56:22 +0200

    chore: tabsize 2

--- apps/stacks-listener/src/app.ts
+++ apps/stacks-listener/src/app.ts
@@ -4,67 +4,67 @@ import * as stacks from "@stacks/blockchain-api-client";
 import { io, Socket } from "socket.io-client";
 
 export class StacksListener {
-    sqs: SQSClient;
-    queueUrl: string;
-    _socket: Socket;
-    listener: stacks.StacksApiSocketClient;
-    private readonly unsubscribeBlocks: () => void;
+  sqs: SQSClient;
+  queueUrl: string;
+  _socket: Socket;
+  listener: stacks.StacksApiSocketClient;
+  private readonly unsubscribeBlocks: () => void;
 
-    constructor(queueUrl: string) {
-        this.sqs = new SQSClient({
-            region: "eu-central-1",
-        });
-        this.queueUrl = queueUrl;
+  constructor(queueUrl: string) {
+    this.sqs = new SQSClient({
+      region: "eu-central-1",
+    });
+    this.queueUrl = queueUrl;
 
-        this._socket = io("https://api.mainnet.hiro.so/", {
-            transports: ["websocket"],
-        });
-        this._socket.on("connect", () =>
-            console.log("socket connected: listening for confirmed blocks...")
-        );
-        this._socket.on("disconnect", () => console.log("socket disconnected"));
-        this._socket.on("connect_error", (err) =>
-            console.log("connection error: ", err)
-        );
+    this._socket = io("https://api.mainnet.hiro.so/", {
+      transports: ["websocket"],
+    });
+    this._socket.on("connect", () =>
+      console.log("socket connected: listening for confirmed blocks...")
+    );
+    this._socket.on("disconnect", () => console.log("socket disconnected"));
+    this._socket.on("connect_error", (err) =>
+      console.log("connection error: ", err)
+    );
 
-        this.listener = new stacks.StacksApiSocketClient(this._socket);
+    this.listener = new stacks.StacksApiSocketClient(this._socket);
 
-        const { unsubscribe } = this.listener.subscribeBlocks(
-            this.sendBlock.bind(this)
-        );
-        this.unsubscribeBlocks = unsubscribe;
+    const { unsubscribe } = this.listener.subscribeBlocks(
+      this.sendBlock.bind(this)
+    );
+    this.unsubscribeBlocks = unsubscribe;
+  }
+
+  dispose() {
+    console.log("closing all websocket connections");
+
+    this.unsubscribeBlocks();
+    this._socket.close();
+  }
+
+  async sendBlock(block: Block) {
+    console.log("Received block ", block.height);
+
+    try {
+      const response = await this.sqs.send(
+        new SendMessageCommand({
+          QueueUrl: this.queueUrl,
+          MessageDeduplicationId: block.hash,
+          MessageGroupId: "stacks-listener",
+          MessageAttributes: {
+            Publisher: {
+              DataType: "String",
+              StringValue: "stacks-listener",
+            },
+          },
+          MessageBody: JSON.stringify(block),
+        })
+      );
+
+      console.log(`Published message ${response.MessageId} to queue.`);
+    } catch (e) {
+      console.error(e);
+      console.error("failed to put block on queue: ", block.hash);
     }
-
-    dispose() {
-        console.log("closing all websocket connections");
-
-        this.unsubscribeBlocks();
-        this._socket.close();
-    }
-
-    async sendBlock(block: Block) {
-        console.log("Received block ", block.height);
-
-        try {
-            const response = await this.sqs.send(
-                new SendMessageCommand({
-                    QueueUrl: this.queueUrl,
-                    MessageDeduplicationId: block.hash,
-                    MessageGroupId: "stacks-listener",
-                    MessageAttributes: {
-                        Publisher: {
-                            DataType: "String",
-                            StringValue: "stacks-listener",
-                        },
-                    },
-                    MessageBody: JSON.stringify(block),
-                })
-            );
-
-            console.log(`Published message ${response.MessageId} to queue.`);
-        } catch (e) {
-            console.error(e);
-            console.error("failed to put block on queue: ", block.hash);
-        }
-    }
+  }
 }
