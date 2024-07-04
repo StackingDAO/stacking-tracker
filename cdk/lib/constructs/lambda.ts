fd3e7973e31756ae3fdcfcaa5b41a8b547d9d111
Niel Deckx <hello@nieldeckx.be>
2024-07-04 11:01:40 +0200

    feat: block processor lambda function + cdk deployment (#10)
    * chore: stacks-listener cleanup
    
    * feat: block processor lambda function + cdk deployment

---
+++ cdk/lib/constructs/lambda.ts
@@ -0,0 +1,45 @@
+import { Duration } from "aws-cdk-lib";
+import { Runtime } from "aws-cdk-lib/aws-lambda";
+import * as lambda from "aws-cdk-lib/aws-lambda-nodejs";
+import { Construct } from "constructs";
+
+export interface LambdaProps {
+  /** The root directory in which the lambda's package.json is situated */
+  lambdaRootDir: string;
+  /** The path from the root directory to the file in which the lambda's handler is */
+  handlerFilePath: string;
+  /** The name of the exproted function that acts as a lambda */
+  handler?: string;
+  /** custom environment variables */
+  environment?: Record<string, string>;
+  /** memory reservation in MB */
+  memory?: number;
+  /** The maximum time the function should run */
+  timeout?: Duration;
+}
+
+export class TypeScriptLambda extends Construct {
+  public readonly props: LambdaProps;
+  public readonly lambda: lambda.NodejsFunction;
+  constructor(scope: Construct, id: string, props: LambdaProps) {
+    super(scope, id);
+    this.props = props;
+
+    this.lambda = new lambda.NodejsFunction(this, "lambda", {
+      entry: this.getHandlerModulePath(
+        props.lambdaRootDir,
+        props.handlerFilePath
+      ),
+      handler: props.handler,
+      timeout: props.timeout || Duration.minutes(10),
+      depsLockFilePath: `${props.lambdaRootDir}/package-lock.json`,
+      runtime: Runtime.NODEJS_20_X,
+      memorySize: props.memory || 512,
+      environment: props.environment,
+    });
+  }
+
+  private getHandlerModulePath(rootFolderPath: string, moduleFileName: string) {
+    return `${rootFolderPath}/${moduleFileName}`;
+  }
+}
---
+++ cdk/lib/tracker.ts
@@ -0,0 +1,52 @@
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as sqs from "aws-cdk-lib/aws-sqs";
+import * as iam from "aws-cdk-lib/aws-iam";
+import { TypeScriptLambda } from "./constructs/lambda";
+import { SqsEventSource } from "aws-cdk-lib/aws-lambda-event-sources";
+
+import "dotenv/config";
+
+export class Tracker extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const queue = new sqs.Queue(this, "Tracker-Blocks-Q", {
+      fifo: true,
+      visibilityTimeout: cdk.Duration.seconds(600), // How long a consumer has to process a message
+      retentionPeriod: cdk.Duration.days(14), // How long message are kept in memory
+      deadLetterQueue: {
+        maxReceiveCount: 3, // Maximum number of receives before moving the message to the DLQ
+        queue: new sqs.Queue(this, "Tracker-Blocks-DLQ", {
+          fifo: true,
+        }),
+      },
+    });
+
+    const wallet_tracker = new TypeScriptLambda(
+      this,
+      "Tracker-Block-Processor",
+      {
+        lambdaRootDir: ".",
+        handlerFilePath: "apps/functions/src/block-processor.ts",
+        handler: "processBlock",
+        environment: {},
+      }
+    );
+
+    wallet_tracker.lambda.addEventSource(new SqsEventSource(queue));
+    wallet_tracker.lambda.addToRolePolicy(
+      new iam.PolicyStatement({
+        actions: ["sns:publish"],
+        resources: ["*"],
+      })
+    );
+
+    new cdk.CfnOutput(this, "WalletTrackeFnrARN", {
+      value: wallet_tracker.lambda.functionArn,
+    });
+    new cdk.CfnOutput(this, "StacksBlocksQueueARN", {
+      value: queue.queueArn,
+    });
+  }
+}
