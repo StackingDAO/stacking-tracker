f083e57bb45148acc8c7b6ce326e3e16aba937d0
Niel Deckx <hello@nieldeckx.be>
2024-07-04 10:39:01 +0200

    feat: add jest to test

---
+++ apps/functions/tests/blockProcessor.test.ts
@@ -0,0 +1,11 @@
+import { processBlock } from "../src/block-processor";
+
+describe("BlockProcessor - ...", () => {
+    // beforeEach
+    // mocks
+
+    test("...", async () => {
+        // processBlock()
+        expect(true).toStrictEqual(true);
+    }, 10000);
+});
