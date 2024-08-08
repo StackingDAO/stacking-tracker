bc7b803ca75b566a568508f27e52a0c41e769cec
Niel Deckx <hello@nieldeckx.be>
2024-08-09 08:29:57 +0900

    fix: web caching

--- web/src/app/common/public-api.ts
+++ web/src/app/common/public-api.ts
@@ -1,7 +1,14 @@
 export const get = async (path: string) => {
-  const res = await fetch(`${process.env.PUBLIC_API_URL}${path.slice(1)}`, {
-    cache: "no-store",
-  });
+  const res = await fetch(
+    `${process.env.PUBLIC_API_URL}${path.slice(1)}`,
+    process.env.NODE_ENV === "development"
+      ? {
+          cache: "no-store",
+        }
+      : {
+          next: { revalidate: 3600 },
+        }
+  );
   const result = await res.json();
   return result;
 };
