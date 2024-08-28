8c6cbf02474563befa892194185b527a01b70ea6
Niel Deckx <hello@nieldeckx.be>
2024-08-28 16:26:39 +0200

    fix: dotenv

--- packages/stacks/src/constants.ts
+++ packages/stacks/src/constants.ts
@@ -1,9 +1,10 @@
 import { Configuration, ConfigurationParameters } from '@stacks/blockchain-api-client';
 import { RateLimiter } from 'limiter';
 
-export const apiUrl = process.env.STACKS_API;
+import * as dotenv from 'dotenv';
+dotenv.config();
 
-console.log('URL', apiUrl);
+export const apiUrl = process.env.STACKS_API;
 
 const limiter = new RateLimiter({ tokensPerInterval: 120, interval: 'second' });
 
