7f75b648e5f56c3f0862a782c3b35ceb309e6adb
Niel Deckx <hello@nieldeckx.be>
2024-09-30 20:22:23 +0200

    fix: fully automate deployments

--- cdk/lib/public-api.ts
+++ cdk/lib/public-api.ts
@@ -2,10 +2,18 @@ import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as ecs from "aws-cdk-lib/aws-ecs";
 import * as ecsPatterns from "aws-cdk-lib/aws-ecs-patterns";
+import * as acm from "aws-cdk-lib/aws-certificatemanager";
+import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
+import * as origins from "aws-cdk-lib/aws-cloudfront-origins";
+import * as route53 from "aws-cdk-lib/aws-route53";
+import * as route53Targets from "aws-cdk-lib/aws-route53-targets";
+import { Duration } from "aws-cdk-lib";
 
 import * as dotenv from "dotenv";
 import { StackSetup } from "./setup";
 dotenv.config({ path: "cdk/.env" });
+
+const domainName = process.env.DOMAIN ?? "stacking-tracker.com";
 
 export class PublicApi extends cdk.Stack {
   constructor(
@@ -46,6 +54,67 @@ export class PublicApi extends cdk.Stack {
       timeout: cdk.Duration.seconds(10), // Timeout for health checks
       healthyThresholdCount: 2, // Number of consecutive successful health checks required
       unhealthyThresholdCount: 5, // Number of consecutive failed health checks required to mark as unhealthy
+    });
+
+    // Certificate and zone for domain
+    const certificate = acm.Certificate.fromCertificateArn(
+      this,
+      "ImportedCert",
+      process.env.CDK_AWS_CERTIFICATE_ARN ?? ""
+    );
+    const hostedZone = route53.HostedZone.fromHostedZoneAttributes(
+      this,
+      "ImportZone",
+      {
+        hostedZoneId: process.env.CDK_AWS_HOSTEDZONE_ID ?? "",
+        zoneName: domainName, // Replace with your actual zone name
+      }
+    );
+
+    // Cache Policy
+    const apiCachePolicy = new cloudfront.CachePolicy(this, "ApiCachePolicy", {
+      cachePolicyName: "ApiCachePolicy",
+      defaultTtl: Duration.seconds(0), // Set the default TTL to 0 seconds
+      minTtl: Duration.seconds(0), // Set the minimum TTL to 0 seconds
+      maxTtl: Duration.seconds(3600), // Set the maximum TTL to 1 hour
+      cookieBehavior: cloudfront.CacheCookieBehavior.none(), // Customize cookie caching
+      headerBehavior: cloudfront.CacheHeaderBehavior.allowList(
+        "Authorization",
+        "Content-Type"
+      ), // Customize header caching
+      queryStringBehavior: cloudfront.CacheQueryStringBehavior.all(), // Customize query string caching
+      enableAcceptEncodingGzip: true,
+      enableAcceptEncodingBrotli: true,
+    });
+
+    // CloudFront Distribution
+    const apiDistribution = new cloudfront.Distribution(
+      this,
+      "ApiDistribution",
+      {
+        defaultBehavior: {
+          origin: new origins.LoadBalancerV2Origin(publicApi.loadBalancer, {
+            protocolPolicy: cloudfront.OriginProtocolPolicy.HTTP_ONLY,
+          }),
+          allowedMethods: cloudfront.AllowedMethods.ALLOW_ALL,
+          cachedMethods: cloudfront.CachedMethods.CACHE_GET_HEAD,
+          cachePolicy: apiCachePolicy,
+          viewerProtocolPolicy:
+            cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+        },
+        domainNames: [`api.${domainName}`],
+        certificate: certificate,
+        minimumProtocolVersion: cloudfront.SecurityPolicyProtocol.TLS_V1_2_2021,
+      }
+    );
+
+    // Add DNS records for the CloudFront distribution
+    new route53.ARecord(this, "CloudFrontApi", {
+      zone: hostedZone,
+      recordName: `api.${domainName}`,
+      target: route53.RecordTarget.fromAlias(
+        new route53Targets.CloudFrontTarget(apiDistribution)
+      ),
     });
   }
 }
