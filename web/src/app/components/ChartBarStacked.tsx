e357ca8a2e66175e1cae552d557f99a57ed7d81f
Niel Deckx <hello@nieldeckx.be>
2024-11-20 18:40:49 +0100

    fix: chart labels and fix for small screens

--- web/src/app/components/ChartBarStacked.tsx
+++ web/src/app/components/ChartBarStacked.tsx
@@ -3,6 +3,7 @@
 import { Bar } from "react-chartjs-2";
 import Chart from "chart.js/auto";
 import { CategoryScale } from "chart.js";
+import { useEffect, useState } from "react";
 
 Chart.register(CategoryScale);
 
@@ -19,6 +20,22 @@ export default function ChartBarStacked({
   chartTitles: ChartTitles;
   chartData: any;
 }) {
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth < 768);
+    };
+
+    handleResize();
+
+    // Add event listener
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <Bar
       data={chartData}
@@ -31,25 +48,28 @@ export default function ChartBarStacked({
           x: {
             stacked: true,
             title: {
-              display: chartTitles.x != undefined,
+              display: false,
               text: chartTitles.x,
             },
           },
           y: {
             stacked: true,
+            display: !isSmallScreen && chartTitles.y != undefined,
+
             title: {
-              display: chartTitles.y != undefined,
+              display: false,
               text: chartTitles.y,
             },
           },
           yRight: {
-            display: chartTitles.yRight != undefined,
+            display: !isSmallScreen && chartTitles.yRight != undefined,
+
             position: "right",
             grid: {
               drawOnChartArea: false,
             },
             title: {
-              display: chartTitles.yRight != undefined,
+              display: false,
               text: chartTitles.yRight,
             },
           },
