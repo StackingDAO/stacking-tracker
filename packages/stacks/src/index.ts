cc88ef8ccc197cb97c82bf4cfb64f2cbcc36eb71
nieldeckx <hello@nieldeckx.be>
2026-03-13 12:08:28 +0100

    fix: packages stacks

--- packages/stacks/src/index.ts
+++ packages/stacks/src/index.ts
@@ -5,6 +5,5 @@ export * from './contracts_stackingdao';
 export * from './contracts_pox';
 export * from './mining';
 export * from './pox';
-export * from './stacks-api';
 export * from './transactions';
 export * from './tokens';
--- packages/stacks/src/transactions.ts
+++ packages/stacks/src/transactions.ts
@@ -1,39 +1,32 @@
-import { BlocksApi, TransactionsApi } from '@stacks/blockchain-api-client';
-import { configuration } from './constants';
+import { stacksApi } from './constants';
 import { cvToJSON, deserializeCV } from '@stacks/transactions';
-
-const transactionsApi = new TransactionsApi(configuration);
 
 export async function getTransactionsByBlockHeight(blockHeight: number): Promise<any> {
   let result: any[] = [];
   let hasReachedEndBlock = false;
 
   while (!hasReachedEndBlock) {
-    const transactions = await transactionsApi.getTransactionsByBlockHeight({
-      height: blockHeight,
-      limit: 50,
-      offset: result.length,
-    });
-    result = result.concat(transactions.results);
+    const { data } = await stacksApi.get(
+      `/extended/v1/tx/block_height/${blockHeight}?limit=50&offset=${result.length}`
+    );
+    result = result.concat(data.results);
 
-    hasReachedEndBlock = transactions.results.length === 0;
+    hasReachedEndBlock = data.results.length === 0;
   }
 
   return result;
 }
 
 export async function getTransactionById(txId: string): Promise<any> {
-  return await transactionsApi.getTransactionById({
-    txId: txId,
-  });
+  const { data } = await stacksApi.get(`/extended/v1/tx/${txId}`);
+  return data;
 }
 
 export async function getAddressTransactions(address: string): Promise<any> {
-  return await transactionsApi.getAddressTransactions({
-    address: address,
-    limit: 50,
-    offset: 0,
-  });
+  const { data } = await stacksApi.get(
+    `/extended/v2/addresses/${address}/transactions?limit=50&offset=0`
+  );
+  return data;
 }
 
 export function reprHexToJson(reprHex: string) {
